f37c8dc4d15e98e2db917f967f867157ed1c4df9
ecokelez <ersoy.cokelez@gmail.com>
2023-02-14 23:13:14 +0200

    JP01

---
+++ src/day11_practice/BatchcConstructor.java
@@ -0,0 +1,18 @@
+package day11_practice;
+
+public class BatchcConstructor {
+    public static void main(String[] args) {
+        // obj create et
+        // method call
+        // sout
+
+        Batch batch=new Batch();
+        System.out.println(batch);  // referans
+        System.out.println("batch.str1 = " + batch.str1);
+
+
+        batch.batch();  // st1 :Batch 81 82 83 Constructor' ı zamanla ANLAYACAKSINIZ
+
+
+    }
+}
