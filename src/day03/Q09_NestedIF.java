7a6d7ac454037a790fcd68dae3045598e1a9de10
Ersoy COKELEZ <ersoy.cokelez@gmail.com>
2023-06-06 23:06:37 +0300

    06.06.2023

--- src/day03/Q09_NestedIF.java
+++ src/day03/Q09_NestedIF.java
@@ -1,5 +1,6 @@
 package day03;
 
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class Q09_NestedIF {
@@ -16,11 +17,19 @@ public class Q09_NestedIF {
 	 */
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Lutfen yas bilginızı gırınız");
-        int yas = scan.nextInt();
+        System.out.println("Please enter your age");
+        int age = scan.nextInt();
 
+        if (age>= 70 ){
+            System.out.println("can vote three times");
+        } else if (age>=50 && age<70) {
+            System.out.println("can vote twice");
+        } else if (age>=18 && age<50) {
+            System.out.println("can vote once");
+        }else if (age>= 18){
+            System.out.println("eligible to vote");
+        }
 
-       }
-
+    }
 
     }
