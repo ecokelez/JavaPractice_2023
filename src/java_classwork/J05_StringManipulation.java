5afb0aecbea7a5a7303e7fc776a4746b38afa7b2
Ersoy COKELEZ <ersoy.cokelez@gmail.com>
2023-06-17 00:14:12 +0300

    review

--- src/java_classwork/J05_StringManipulation.java
+++ src/java_classwork/J05_StringManipulation.java
@@ -2,7 +2,7 @@ package java_classwork;
 
 import java.util.Scanner;
 
-public class J05 {
+public class J05_StringManipulation {
 
     // Ask the user for a four-letter hand and
     // Print the entered word backwards
@@ -11,13 +11,22 @@ public class J05 {
         System.out.println("Please enter four-letter word");
         String str = scan.nextLine();
 
-        if (str.length()<=4){
+       // 1. method :
+        if (str.length()==4){
            char one = str.charAt(0);
            char two = str.charAt(1);
            char three = str.charAt(2);
            char four = str.charAt(3);
             System.out.println("Str : " + one+two+three+four);
         } else System.out.println("Please enter a  valid word");
+
+        // 2. method :
+       String reverse = str.substring(3)+
+               str.substring(2,3)+
+               str.substring(1,2)+
+               str.substring(0,1);
+        System.out.println("reverse : " + reverse);
+
 
     }
 }
